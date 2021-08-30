685400c62c1fd8215408b457ab839ef0cdab46bc
Virus936 <lok.vira@gmail.com>
2021-08-30 16:05:05 +0200

    searchBar

--- src/components/searchbar/SearchBar.js
+++ src/components/searchbar/SearchBar.js
@@ -6,9 +6,10 @@ import skills from '../skills'
 
 function SearchBar(){
   const [keyword, setKeyword] = useState("")
-  const [selector, setSelector] = useState("android")
-  const [localisation, setLocalisation] = useState("qwe")
+  const [selector, setSelector] = useState("")
+  const [localisation, setLocalisation] = useState("")
   const [active, setActive] = useState(false)
+  const [filter, setFilter] = useState("")
 
   return(
     <Container>
@@ -19,11 +20,21 @@ function SearchBar(){
           <input id='keyword' type="text" placeholder='Mot-clés' value={keyword} name='keyword' onChange={e => setKeyword(e.target.value)}/>
         </div>
 
-      <div onClick={e => setActive(!active)}>
-          <input type="text" value={selector} name='selector' />
+      <div >
+          <input onClick={e => setActive(!active)} placeholder="Spécilalités" type="text" value={selector} name='selector' />
           <div className={ `scrollSelector ${active&&'active'}` }>
-            { skills.map(e => {
-              return <div key={e} onClick={i=> setSelector(e)}>{e}</div>
+            <div className='searchSelect'>
+            <BiSearch />
+              <input type="text" value={filter} onChange={ e => { setFilter(e.target.value) } } />
+            </div>
+            { skills.filter(skill => {
+              if( filter === "" ){
+                return skill
+              }else if(skill.toLowerCase().includes(filter.toLowerCase())){
+                return skill
+              }
+            }).map(e => {
+              return <div key={e} onClick={i=> {setActive(!active); setSelector(e) }}>{e}</div>
               })
             }
           </div>
@@ -33,12 +44,16 @@ function SearchBar(){
           <input type="text" onChange={e=>setLocalisation(e.target.value)} value={localisation} name="localisation" />
         </div>
 
-        <button> chercher </button>
+        <div className="submitButton">
+          <input type="submit" value="Chercher"/>
+        </div>
     </Container>
     )
 }
 const Container = styled.form`
   display:flex;
+  position:relative;
+  top:-30px;
   align-items:start;
   height:2.5em;
   background-color: rgb(245,246,248);
@@ -59,8 +74,17 @@ const Container = styled.form`
     margin-left:.5em;
     outline:none;
     height:100%;
+    width:140px;
     font-size:16px;
     height:2em;
+  }
+  .submitButton{
+    display:flex;
+    align-items:center;
+    background-color:pink;
+    input{
+      margin:0 30px;
+    }
   }
 
   &>div{
@@ -88,6 +112,9 @@ const Container = styled.form`
       }
     }
 
+  }
+  .searchSelect{
+    display:flex;
   }
 `
 
